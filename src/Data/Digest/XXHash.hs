e4b0269c68f8729cc2b4427d72e6cff92b233f16
Christian Marie <pingu@anchor.net.au>
2014-01-08 20:09:30 +1100

    Oh god what? We run faster without unboxing and primitive operations if we turn OFF optimizations? You confuse me GHC :(

--- src/Data/Digest/XXHash.hs
+++ src/Data/Digest/XXHash.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE CPP, GeneralizedNewtypeDeriving, RecordWildCards #-}
-{-# LANGUAGE UnboxedTuples, MagicHash #-}
+{-# LANGUAGE BangPatterns, CPP, GeneralizedNewtypeDeriving, RecordWildCards #-}
+{-# OPTIONS_GHC -funbox-strict-fields #-}
 
 module Data.Digest.XXHash
 (
+    Acc(..),
     fromAcc,
     stageThree,
     stageTwo,
@@ -14,19 +15,23 @@ module Data.Digest.XXHash
 #include "MachDeps.h"
 
 import Data.Bits
+import Data.Word (Word8, Word32)
 import Foreign.ForeignPtr (withForeignPtr)
-import Foreign.Ptr
+import Foreign.Ptr (Ptr, castPtr, plusPtr)
 import Data.ByteString.Internal (ByteString(PS), inlinePerformIO)
 import Foreign.Storable (peek)
 
-import GHC.Prim
-import GHC.Word
+data Acc = Acc { a1 :: !Word32
+               , a2 :: !Word32
+               , a3 :: !Word32
+               , a4 :: !Word32
+               } deriving (Eq, Show)
 
 type XXHash = Word32
 type Seed = Word32
 
-initState :: Seed -> (# Word32, Word32, Word32, Word32 #)
-initState seed = (#,,,#) a1 a2 a3 a4
+initState :: Seed -> Acc
+initState seed = Acc a1 a2 a3 a4
     where a1 = seed + prime1 + prime2
           a2 = seed + prime2
           a3 = seed
@@ -45,30 +50,12 @@ finalize hash = step2 `xor` step2 `shiftR` 16
     step1 = (hash `xor` (hash `shiftR` 15)) * prime2
     step2 = (step1 `xor` (step1 `shiftR` 13)) * prime3
 
-
--- stageOne uses primitive operations to reduce allocations by allowing inlined
--- words to remain boxed.
-stageOne :: Word# -> Word# -> Word# -> Word# 
-         -> Word# -> Word# -> Word# -> Word#
-         -> (# Word#, Word#, Word#, Word# #)
-stageOne i1 i2 i3 i4 a1 a2 a3 a4 =
-    (# ,,, #) (vx a1 i1) (vx a2 i2) (vx a3 i3) (vx a4 i4)
+stageOne :: Word32 -> Word32 -> Word32 -> Word32 -> Acc -> Acc
+stageOne i1 i2 i3 i4 Acc{..} =
+    Acc (vx a1 i1) (vx a2 i2) (vx a3 i3) (vx a4 i4)
   where
-      -- vx v i = ((v + i * prime2) `rotateL` 13) * prime1
-      vx v i = let x :: Word#
-                   x = narrow32Word#
-                         (plusWord#
-                            v
-                            (narrow32Word#
-                              (timesWord# i (2246822519##))))
-                in narrow32Word#
-                     (timesWord#
-                        (narrow32Word#
-                          (or#
-                             (uncheckedShiftL# x 13#)
-                             (uncheckedShiftRL# x 19#)))
-                        (2654435761##))
-
+      vx v i = ((v + i * prime2) `rotateL` 13) * prime1
+    
 stageTwo :: Word32 -> XXHash -> XXHash
 stageTwo i hash =
     ((hash + i * prime3) `rotateL` 17) * prime4
@@ -77,8 +64,8 @@ stageThree :: Word8 -> XXHash -> XXHash
 stageThree i hash =
     ((hash + fromIntegral i * prime5) `rotateL` 11) * prime1
 
-fromAcc :: Word32 -> Word32 -> Word32 -> Word32 -> XXHash
-fromAcc a1 a2 a3 a4 = (a1 `rotateL` 1) + (a2 `rotateL` 7) + (a3 `rotateL` 12) + (a4 `rotateL` 18)
+fromAcc :: Acc -> XXHash
+fromAcc Acc{..} = (a1 `rotateL` 1) + (a2 `rotateL` 7) + (a3 `rotateL` 12) + (a4 `rotateL` 18)
 
 {-# INLINE fromAcc #-}
 {-# INLINE stageThree #-}
@@ -92,21 +79,18 @@ hashByteString seed (PS fp os len) =
   inlinePerformIO . withForeignPtr fp $ \bs_base_ptr ->
     let ptr_beg = bs_base_ptr `plusPtr` os
         ptr_end = ptr_beg `plusPtr` len
-        processBody :: Ptr Word8 -> Word# -> Word# -> Word# -> Word# -> IO XXHash
-        processBody ptr a1 a2 a3 a4
+        processBody :: Ptr Word8 -> Acc -> IO XXHash
+        processBody ptr !v
             | ptr <= ptr_end `plusPtr` (-16) = do
-                (W32# i1) <- peekLE32 ptr 0
-                (W32# i2) <- peekLE32 ptr 4
-                (W32# i3) <- peekLE32 ptr 8
-                (W32# i4) <- peekLE32 ptr 12
-                let (# a1', a2', a3', a4' #) = stageOne i1 i2 i3 i4 a1 a2 a3 a4
-                processBody (ptr `plusPtr` 16) a1' a2' a3' a4'
-            | otherwise =
-                processEnd ptr $
-                    (fromAcc (W32# a1) (W32# a2) (W32# a3) (W32# a4)) +
-                        (fromIntegral len)
+                i1 <- peekLE32 ptr 0
+                i2 <- peekLE32 ptr 4
+                i3 <- peekLE32 ptr 8
+                i4 <- peekLE32 ptr 12
+                processBody (ptr `plusPtr` 16) (stageOne i1 i2 i3 i4 v)
+            | otherwise = do
+                processEnd ptr $ (fromAcc v) + (fromIntegral len)
         processEnd :: Ptr Word8 -> XXHash -> IO XXHash
-        processEnd ptr hash
+        processEnd ptr !hash
             | ptr <= ptr_end `plusPtr` (-4) = do
                 b <- peekLE32 ptr 0
                 processEnd (ptr `plusPtr` 4) (stageTwo b hash)
@@ -117,8 +101,7 @@ hashByteString seed (PS fp os len) =
                 processEnd (ptr `plusPtr` 1) (stageThree b hash)
     in if len < 16
        then processEnd ptr_beg $ prime5 + seed + fromIntegral len -- shortcut
-       else let (# W32# a1', W32# a2', W32# a3', W32# a4' #) = initState seed in
-                processBody ptr_beg a1' a2' a3' a4'
+       else processBody ptr_beg $ initState seed
 
 peekByte :: Ptr Word8 -> IO Word8
 peekByte = peek
@@ -126,7 +109,7 @@ peekByte = peek
 
 peekLE32 :: Ptr Word8 -> Int -> IO Word32
 #if defined(x86_64_HOST_ARCH) || defined(i386_HOST_ARCH)
-peekLE32 ptr os =  peek (castPtr (ptr `plusPtr` os))
+peekLE32 ptr os = peek (castPtr (ptr `plusPtr` os))
 #else
 -- Super slow, around 100x slower.
 peekLE32 ptr os = do
@@ -136,7 +119,7 @@ peekLE32 ptr os = do
     b2 <- peek8 2
     b3 <- peek8 3
     let w = (b3 `shiftL` 24) .|. (b2 `shiftL` 16) .|.
-            (b1 `shiftL` 8)  .|. b0
+            (b1 `shiftL` 8) .|. b0
     return w
 #endif
 {-# INLINE peekLE32 #-}
--- tests/TestSuite.hs
+++ tests/TestSuite.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE OverloadedStrings, UnboxedTuples, MagicHash #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module TestSuite where
 
 import Test.Hspec
 
 import Data.Digest.XXHash
-import GHC.Word
 
 suite :: Spec
 suite = do
@@ -14,6 +13,8 @@ suite = do
         it "works with known pairs" $ do
             hashByteString 0 "the quick brown fox jumps over a pony" `shouldBe`
                 2069436874
+            hashByteString 0 "small" `shouldBe`
+                2387791210
             hashByteString 0 "small" `shouldBe` 2387791210
             hashByteString 123 "test" `shouldBe` 2758658570
             hashByteString 42 "b" `shouldBe` 2220134325
@@ -21,7 +22,8 @@ suite = do
 internalFunctionsTest :: Spec
 internalFunctionsTest = do
     it "fromAcc has correct output" $
-        3672751773 `shouldBe` fromAcc 3707864141 1226176970 2466439235 1709939193
+        3672751773 `shouldBe` fromAcc 
+            (Acc 3707864141 1226176970 2466439235 1709939193)
     it "stageOne has correct output" testStageOne
     it "stageTwo has correct output" $
         stageTwo 1852796960 3672751810 `shouldBe` 892131078
@@ -30,10 +32,9 @@ internalFunctionsTest = do
     it "finalize has correct output" $
         finalize 1167152160 `shouldBe` 2069436874
   where
-    testStageOne = do
-        let (# a1, a2, a3, a4 #) = stageOne 543516788## 1667855729## 1919033451## 544110447##
-                                            606290984## 2246822519## 0##          1640531535## 
-        W32# a1 `shouldBe` 2590359605
-        W32# a2 `shouldBe` 2072081322
-        W32# a3 `shouldBe` 3418880061
-        W32# a4 `shouldBe` 977135912
+    testStageOne =
+        r `shouldBe` stageOne 543516788 1667855729 1919033451 544110447 v
+      where
+        
+        v = Acc 606290984 2246822519 0 1640531535
+        r = Acc 2590359605 2072081322 3418880061 977135912
